f14ba4e059b96eb4e136a57f7c1f86efdb6a13c2
Paul E. Sevinç <paul@squeng.com>
2021-01-11 11:42:09 +0100

    prepared the back-end for delivering the front-end's index.html

--- beuawg/app/controllers/HomeController.scala
+++ beuawg/app/controllers/HomeController.scala
@@ -1,24 +1,22 @@
 package controllers
 
-import javax.inject._
-import play.api._
-import play.api.mvc._
+import javax.inject.Inject
+import javax.inject.Singleton
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+import play.api.Environment
+import play.api.mvc.AnyContent
+import play.api.mvc.BaseController
+import play.api.mvc.ControllerComponents
+import play.api.mvc.Request
+
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject() (
+    val controllerComponents: ControllerComponents,
+    val env: Environment
+) extends BaseController {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+    implicit val ec = scala.concurrent.ExecutionContext.global
+    Ok.sendResource("public/index.html", env.classLoader)
   }
 }
