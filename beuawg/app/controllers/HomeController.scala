73624d3b479037fe2b0cd01cb02377ece2740563
Paul E. Sevinç <paul@squeng.com>
2021-01-13 23:02:06 +0100

    hardened the back-end

--- beuawg/app/controllers/HomeController.scala
+++ beuawg/app/controllers/HomeController.scala
@@ -3,11 +3,15 @@ package controllers
 import javax.inject.Inject
 import javax.inject.Singleton
 
+import scala.io.Codec
+import scala.io.Source
+
 import play.api.Environment
 import play.api.mvc.AnyContent
 import play.api.mvc.BaseController
 import play.api.mvc.ControllerComponents
 import play.api.mvc.Request
+import play.filters.csrf.CSRF
 
 @Singleton
 class HomeController @Inject() (
@@ -16,7 +20,16 @@ class HomeController @Inject() (
 ) extends BaseController {
 
   def index() = Action { implicit request: Request[AnyContent] =>
-    implicit val ec = scala.concurrent.ExecutionContext.global
-    Ok.sendResource("public/index.html", env.classLoader)
+    val is = env.classLoader.getResourceAsStream("public/index.html")
+    val bufferedSource = Source.createBufferedSource(
+      inputStream = is,
+      close = () => is.close()
+    )(Codec.UTF8)
+    val stringBuilder = bufferedSource.addString(new StringBuilder())
+    val string = stringBuilder.mkString
+    val token =
+      CSRF.getToken // // https://www.playframework.com/documentation/latest/ScalaCsrf#Getting-the-current-token
+    Ok(string.replace("REPLACE_CSRF_TOKEN", token.get.value))
+      .as("text/html")
   }
 }
