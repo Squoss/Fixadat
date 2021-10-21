94f6a577aab0e900f9442a9a00afa7a59c7c8500
Paul E. Sevinç <paul@squeng.com>
2021-10-21 10:25:55 +0200

    updated the dependencies

--- feuawg/src/MapDisplayClass.jsx
+++ feuawg/src/MapDisplayClass.jsx
@@ -13,7 +13,7 @@ export default class MapDisplayClass extends React.Component {
     return this.props.value === undefined ? <div /> :
       <React.Fragment>
         <a href={`https://wego.here.com/?map=${this.props.value.latitude},${this.props.value.longitude},${zoomLevel}`} target="HEREWeGo">
-          <img alt={`a map${this.props.value.name ? ` of ${this.props.value.name}` : ""}`} src={`https://image.maps.ls.hereapi.com/mia/1.6/mapview?apiKey=${apiKey}&c=${this.props.value.latitude},${this.props.value.longitude}&h=540&w=960&z=${zoomLevel}`} />;
+          <img alt={`a map${this.props.value.name ? ` of ${this.props.value.name}` : ""}`} src={`https://image.maps.ls.hereapi.com/mia/1.6/mapview?apiKey=${apiKey}&c=${this.props.value.latitude},${this.props.value.longitude}&h=540&w=960&z=${zoomLevel}`} />
         </a>
       </React.Fragment>
   }
