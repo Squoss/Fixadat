f6fcaf0811ccbc479212872a49e8d85013cdfb30
Paul E. Sevinç <paul@squeng.com>
2023-12-01 14:59:14 +0100

    wip

--- fegui/vite.config.ts
+++ fegui/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(() => {
   return {
     build: {
-      assetsDir: 'fegui',
+      assetsDir: 'vrassets',
       // assetsInlineLimit: 0, <- had hoped to mimic INLINE_RUNTIME_CHUNK=false (https://create-react-app.dev/docs/advanced-configuration/)
       outDir: 'build',
     },
@@ -14,5 +14,14 @@ export default defineConfig(() => {
         // string shorthand: http://localhost:5173/iapi -> http://localhost:9000/iapi
         '/iapi': 'http://localhost:9000',
       },
-    },  };
+    },
+    experimental: {
+      // https://vitejs.dev/guide/build.html#advanced-base-options
+      renderBuiltUrl(filename: string, { hostId, hostType, type }: { hostId: string, hostType: 'js' | 'css' | 'html', type: 'public' | 'asset' }) {
+        if (type === 'asset' || type === 'public') {
+          return 'fegui/' + filename
+        }
+      }
+    }
+  };
 });
